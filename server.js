48e66fcee63d602f6249003dc89889ab5ed9508a
Shirley Cheung <shirleycheung315@gmail.com>
2021-01-07 19:41:09 -0500

    test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 app.use(cors());
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", (req, res) => {
   res.send("hello");
